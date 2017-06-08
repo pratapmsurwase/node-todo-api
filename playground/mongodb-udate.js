981360bad8c99926a3745205757548ec54b23dcc
surwase <pratap7684@gmail.com>
2017-06-08 15:46:23 +0530

    message

--- playground/mongodb-udate.js
+++ playground/mongodb-udate.js
@@ -7,10 +7,24 @@ MongoClient.connect ('mongodb://localhost:27017/TodoApp',  (err,  db)   => {
   }
   console.log('Connected to Mongo DB');
 // delete many
-db.collection('UserData').findOneAndUpdate({_id: new ObjectId("593901c4157df535831f1d65")
+// db.collection('UserData').findOneAndUpdate({_id: new ObjectId("593901c4157df535831f1d65")
+// },    {
+//   $set: {
+//     completed: true
+//   }
+// },   {
+//    returnOriginal: false
+// }).then((result)  => {
+//   console.log(result);
+// });
+
+db.collection('UserData').findOneAndUpdate({  _id: new ObjectId("59390a85157df535831f1ef1")
 },    {
   $set: {
-    completed: true
+    Name: 'Pratap'
+  },
+  $inc:  {
+    Age: 1
   }
 },   {
    returnOriginal: false
